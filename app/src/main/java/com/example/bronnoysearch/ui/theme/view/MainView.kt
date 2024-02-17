de4f3baeab8aae413b1b60d8f01408e99c81bec7
Vilde Hagtvedt <54098897+vildeHagt@users.noreply.github.com>
2024-02-17 21:17:05 +0100

    Implementerr bilder

--- app/src/main/java/com/example/bronnoysearch/ui/theme/view/MainView.kt
+++ app/src/main/java/com/example/bronnoysearch/ui/theme/view/MainView.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bronnoysearch.R
 import com.example.bronnoysearch.ui.theme.LightBeige
 import com.example.bronnoysearch.ui.theme.MellowBlue
@@ -31,8 +32,14 @@ fun MainView() {
         bottomBar = {
             BottomAppBar(containerColor = MellowBlue) {
                 Image(
-                    modifier = Modifier.fillMaxSize(0.5f),
+                    modifier = Modifier.fillMaxSize().weight(1f).padding(10.dp),
                     painter = painterResource(id = R.drawable.search_icon),
+                    contentDescription = null
+                )
+
+                Image(
+                    modifier = Modifier.fillMaxSize().weight(1f),
+                    painter = painterResource(id = R.drawable.load_icon),
                     contentDescription = null
                 )
             }
